fcd1735c30260d221f288bc17829ea573bfe3753
IgorVoskresensky <99877699+IgorVoskresensky@users.noreply.github.com>
2023-02-16 15:55:27 +0300

    Firebase added

--- app/src/main/java/ru/ivos/quantron_movies/utils/Extensions.kt
+++ app/src/main/java/ru/ivos/quantron_movies/utils/Extensions.kt
@@ -1,9 +1,13 @@
 package ru.ivos.quantron_movies.utils
 
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import ru.ivos.quantron_movies.R
+import ru.ivos.quantron_movies.data.db.FavoriteMapper
+import ru.ivos.quantron_movies.data.models.favorite.Favorite
+import ru.ivos.quantron_movies.data.models.movie.MovieDetails
 
 fun AppCompatActivity.replaceFragment(fragment: Fragment, addToBackStack: Boolean = true) {
     if(addToBackStack) {
@@ -37,4 +41,8 @@ fun View.visible() {
 
 fun View.gone() {
     visibility = View.GONE
+}
+
+fun mapToMovieDetails(list: List<Favorite>): List<MovieDetails> {
+    return list.map { FavoriteMapper().mapFavoriteDetailsToMovieDetails(it) }
 }
